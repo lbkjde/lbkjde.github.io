a876d8a4d251977e469582c3f9fb2d1cd71e6f1b
LBKinson <lkinson@fender.com>
2017-07-28 16:49:04 -0700

    conditionalizing for pages without flickity

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-window.addEventListener( 'load', function() {
-  new Flickity( '.carousel', {
-    cellAlign: 'left',
-    wrapAround: true
+if (window.$('.carousel')) {
+  document.addEventListener( 'load', function() {
+    new Flickity( '.carousel', {
+      cellAlign: 'left',
+      wrapAround: true
+    });
   });
-  new Flickity( '.carousel-2', {
-    cellAlign: 'left',
-    wrapAround: true
+}
+if (window.$('.carousel-2')) {
+  document.addEventListener( 'load', function() {
+    new Flickity( '.carousel-2', {
+      cellAlign: 'left',
+      wrapAround: true
+    });
   });
-});
+}
