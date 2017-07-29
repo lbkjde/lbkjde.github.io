d7aba63d483ba9c44fb81ed3acc320da1c2c8d00
LBKinson <lkinson@fender.com>
2017-07-28 17:35:36 -0700

    forever.

--- js/main.js
+++ js/main.js
@@ -14,3 +14,11 @@ if (window.$('.carousel-2')[0]) {
     });
   });
 }
+if (window.$('.carousel-3')[0]) {
+  window.addEventListener( 'load', function() {
+    new Flickity( '.carousel-3', {
+      cellAlign: 'left',
+      wrapAround: true
+    });
+  });
+}
