c98157b453c7848a8f5e79837db0c68c082b7a30
LBKinson <lkinson@fender.com>
2017-07-28 17:38:54 -0700

    trying a different conditional

--- js/main.js
+++ js/main.js
@@ -1,24 +1,20 @@
-if (window.$('.carousel')[0]) {
-  window.addEventListener( 'load', function() {
+window.addEventListener( 'load', function() {
+  if (window.$('.carousel')[0]) {
     new Flickity( '.carousel', {
       cellAlign: 'left',
       wrapAround: true
     });
-  });
-}
-if (window.$('.carousel-2')[0]) {
-  window.addEventListener( 'load', function() {
+  }
+  if (window.$('.carousel-2')[0]) {
     new Flickity( '.carousel-2', {
       cellAlign: 'left',
       wrapAround: true
     });
-  });
-}
-if (window.$('.carousel-3')[0]) {
-  window.addEventListener( 'load', function() {
+  }
+  if (window.$('.carousel-3')[0]) {
     new Flickity( '.carousel-3', {
       cellAlign: 'left',
       wrapAround: true
     });
-  });
-}
+  }
+});
